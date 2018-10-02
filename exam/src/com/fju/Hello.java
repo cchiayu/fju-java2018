f07cb0d668955c36d2effbe833001695f75fc3cc
cchiayu <user@es302n09>
2018-10-02 11:04:48 +0800

    initial

---
+++ exam/src/com/fju/Hello.java
@@ -0,0 +1,16 @@
+package com.fju;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    System.out.println("Hello");
+    int age = 19;
+    float weight = 52.5f;
+    System.out.println(weight);
+    System.out.println(age);
+    String name = "hey";
+    System.out.println(name);
+	}
+
+}
---
+++ myproject/src/com/tom/Hello.java
@@ -0,0 +1,17 @@
+package com.tom;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Hello world");
+        int age = 19;
+        String name = "melissa";
+        System.out.println(name);		
+        int schoolYear = 107;		
+        float weight = 52.5f;		
+        System.out.println(schoolYear);
+        System.out.println(weight);
+        
+    }
+}
