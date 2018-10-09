e9e7fa1138f6137c5e2dc94c0d7436c617361931
user <user@es302n36>
2018-10-09 12:01:26 +0800

    10/9

--- myproject/src/com/tom/Hello.java
+++ myproject/src/com/tom/Hello.java
@@ -6,20 +6,30 @@ public class Hello {
 		// TODO Auto-generated method stub
         System.out.println("Hello world");
         int age = 18;
-        System.out.println(age);
         String name = "melissa";
-        System.out.println(name);		
         int schoolYear = 107;		
-        float weight = 45.5f;		
-        System.out.println(schoolYear);
-        System.out.println(weight);
+        float weight = 45.5f;
         float num = 12.2f;
-        System.out.println(num);
         float height = 1.62f;
         float bmi = weight / (height*height);
-        System.out.println(bmi);
         age = age + 1;
         System.out.println(age);
+        System.out.println(name);				
+        System.out.println(schoolYear);
+        System.out.println(weight);
+        System.out.println(num);
+        System.out.println(bmi);
+        System.out.println(age);
+        Person P = new Person(height, weight);
+        P.height = 1.82f;
+        P.weight = 60.5f;
+        System.out.println(bmi);   
+        
+      
+        
+        
+        
+        
         
         
         
---
+++ myproject/src/com/tom/Person.java
@@ -0,0 +1,16 @@
+package com.tom;
+
+public class Person {
+	float height;
+	float weight;
+
+	public Person(float weight, float height) {
+		this.height = height;
+		this.weight = weight;
+		
+
+		
+	}
+
+	
+	}
