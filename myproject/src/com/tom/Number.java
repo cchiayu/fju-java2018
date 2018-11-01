0ef9b95a9d2264961fe33b811447a1cc95554ffc
Chiayu <Chiayu@LAPTOP-VN7Q9I4R>
2018-11-01 21:09:33 +0800

    11/1練習數字

--- myproject/src/com/tom/Number.java
+++ myproject/src/com/tom/Number.java
@@ -1,19 +1,31 @@
 package com.tom;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class Number {
 
 	public static void main(String[] args) {
-		System.out.println("please enter a number from 1 to 100");
+		
+		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
 		int secret = random.nextInt(100)+1;
 		System.out.println(secret);
-		while (! = secret) {
-			
+		System.out.println("please enter a number from 1 to 100");
+		int min = 1;
+		int max = 100;
+		int guess = scanner.nextInt();
+		while (secret!=guess ) {
+			if (guess>secret&&guess<=100) {
+				max = guess;
+			}else {
+				min = guess;
+			}
+			System.out.printf(min+"to"+ max,"please enter number:");
+			guess = scanner.nextInt();//guess again
 		}
 		
-		
+		System.out.println("®¥³ßµª¹ï¤F!!");
 		
 		
 		int a = 1;
@@ -21,12 +33,6 @@ public class Number {
 		System.out.println("yayayay");
 		a = a - 1;
 		}
-			
-
-		
-		
-		
 		
 	}
-
 }
